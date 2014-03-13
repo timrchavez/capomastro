47c82897f64027482cf42071ff3151afc0454ce1
Kevin McDermott <kevin@bigkevmcd.com>
2014-03-13 15:52:21 +0000

    Restore import_builds_for_job.

--- jenkins/helpers.py
+++ jenkins/helpers.py
@@ -37,41 +37,24 @@ def import_build_for_job(job_id, build_number):
         Artifact.objects.create(**artifact_details)
 
 
-#def import_builds_for_job(job_id):
-#    """
-#    Import all Builds for a job using the job_id.
-#    """
-#    job = Job.objects.get(pk=job_id)
-#
-#    logging.info("Located job %s\n" % job)
-#
-#    client = job.server.get_client()
-#
-#    logging.info("Using server at %s\n" % job.server.url)
-#
-#    jenkins_job = client.get_job(job.name)
-#
-#    good_build_ids = list(jenkins_job.get_build_ids())
-#    logging.info("%s\n" % good_build_ids)
-#
-#    for build_id in good_build_ids:
-#        build_result = jenkins_job.get_build(build_id)
-#        # TODO: Shouldn't access _data here.
-#        build_details = {
-#            "job": job,
-#            "status": build_result.get_status(),
-#            "build_id": build_result._data["id"],
-#            "number": build_result.buildno,
-#            "duration": build_result._data["duration"],
-#            "url": build_result.get_result_url()
-#        }
-#        logging.info("%s" % build_details)
-#        build = Build.objects.create(**build_details)
-#        for artifact in build_result.get_artifacts():
-#            artifact_details = {
-#                "filename": artifact.filename,
-#                "url": artifact.url,
-#                "build": build
-#            }
-#            logging.info("%s" % artifact_details)
-#            Artifact.objects.create(**artifact_details)
+def import_builds_for_job(job_id):
+    """
+    Import all Builds for a job using the job_id.
+
+    TODO: Add testing - only used by command-line tool just now.
+    """
+    job = Job.objects.get(pk=job_id)
+
+    logging.info("Located job %s\n" % job)
+
+    client = job.server.get_client()
+
+    logging.info("Using server at %s\n" % job.server.url)
+
+    jenkins_job = client.get_job(job.name)
+
+    good_build_numbers = list(jenkins_job.get_build_ids())
+    logging.info("%s\n" % good_build_numbers)
+
+    for build_number in good_build_numbers:
+        import_build_for_job(job.pk, build_number)
