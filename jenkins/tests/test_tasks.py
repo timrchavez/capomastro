b99faa1ac4efbd613a5ac97858b0950402162ba4
Kevin McDermott <kevin@bigkevmcd.com>
2014-03-18 11:54:08 +0000

    Use spec properly.

--- jenkins/tests/test_tasks.py
+++ jenkins/tests/test_tasks.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.test.utils import override_settings
 
 import mock
+from jenkinsapi import jenkins
 
 from jenkins.tasks import build_job, push_job_to_jenkins
 from .factories import (
@@ -20,7 +21,7 @@ class BuildJobTaskTest(TestCase):
         the job be built.
         """
         job = JobFactory.create(server=self.server)
-        with mock.patch("jenkins.models.Jenkins", spec=True) as mock_jenkins:
+        with mock.patch("jenkins.models.Jenkins", spec=jenkins.Jenkins) as mock_jenkins:
             build_job(job.pk)
 
         mock_jenkins.assert_called_with(
@@ -34,7 +35,7 @@ class BuildJobTaskTest(TestCase):
         If we provide a build_id, this should be sent as parameter.
         """
         job = JobFactory.create(server=self.server)
-        with mock.patch("jenkins.models.Jenkins", spec=True) as mock_jenkins:
+        with mock.patch("jenkins.models.Jenkins", spec=jenkins.Jenkins) as mock_jenkins:
             build_job(job.pk, "20140312.1")
 
         mock_jenkins.assert_called_with(
@@ -64,7 +65,7 @@ class CreateJobTaskTest(TestCase):
         """
         jobtype = JobTypeFactory.create(config_xml=job_xml)
         job = JobFactory.create(jobtype=jobtype, name="testing")
-        with mock.patch("jenkins.models.Jenkins", spec=True) as mock_jenkins:
+        with mock.patch("jenkins.models.Jenkins", spec=jenkins.Jenkins) as mock_jenkins:
             push_job_to_jenkins(job.pk)
 
         mock_jenkins.assert_called_with(
