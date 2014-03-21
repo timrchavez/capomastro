0a80a10595149018470361a0ba1012fa43aed196
Kevin McDermott <kevin@bigkevmcd.com>
2014-03-21 08:31:22 +0000

    Reset the migrations.

--- jenkins/migrations/0001_initial.py
+++ jenkins/migrations/0001_initial.py
@@ -19,13 +19,26 @@ class Migration(SchemaMigration):
         ))
         db.send_create_signal(u'jenkins', ['JenkinsServer'])
 
+        # Adding model 'JobType'
+        db.create_table(u'jenkins_jobtype', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('description', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
+            ('config_xml', self.gf('django.db.models.fields.TextField')()),
+        ))
+        db.send_create_signal(u'jenkins', ['JobType'])
+
         # Adding model 'Job'
         db.create_table(u'jenkins_job', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('server', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['jenkins.JenkinsServer'])),
-            ('name', self.gf('django.db.models.fields.CharField')(max_length=255, blank=True)),
+            ('jobtype', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['jenkins.JobType'])),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=255)),
         ))
         db.send_create_signal(u'jenkins', ['Job'])
+
+        # Adding unique constraint on 'Job', fields ['server', 'name']
+        db.create_unique(u'jenkins_job', ['server_id', 'name'])
 
         # Adding model 'Build'
         db.create_table(u'jenkins_build', (
@@ -37,6 +50,7 @@ class Migration(SchemaMigration):
             ('url', self.gf('django.db.models.fields.CharField')(max_length=255)),
             ('phase', self.gf('django.db.models.fields.CharField')(max_length=25)),
             ('status', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('console_log', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
         ))
         db.send_create_signal(u'jenkins', ['Build'])
 
@@ -51,8 +65,14 @@ class Migration(SchemaMigration):
 
 
     def backwards(self, orm):
+        # Removing unique constraint on 'Job', fields ['server', 'name']
+        db.delete_unique(u'jenkins_job', ['server_id', 'name'])
+
         # Deleting model 'JenkinsServer'
         db.delete_table(u'jenkins_jenkinsserver')
+
+        # Deleting model 'JobType'
+        db.delete_table(u'jenkins_jobtype')
 
         # Deleting model 'Job'
         db.delete_table(u'jenkins_job')
@@ -75,6 +95,7 @@ class Migration(SchemaMigration):
         u'jenkins.build': {
             'Meta': {'ordering': "['-number']", 'object_name': 'Build'},
             'build_id': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'console_log': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
             'duration': ('django.db.models.fields.IntegerField', [], {'null': 'True'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'job': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['jenkins.Job']"}),
@@ -93,10 +114,18 @@ class Migration(SchemaMigration):
             'username': ('django.db.models.fields.CharField', [], {'max_length': '255'})
         },
         u'jenkins.job': {
-            'Meta': {'object_name': 'Job'},
+            'Meta': {'unique_together': "(('server', 'name'),)", 'object_name': 'Job'},
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'jobtype': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['jenkins.JobType']"}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
             'server': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['jenkins.JenkinsServer']"})
+        },
+        u'jenkins.jobtype': {
+            'Meta': {'object_name': 'JobType'},
+            'config_xml': ('django.db.models.fields.TextField', [], {}),
+            'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'})
         }
     }
 
