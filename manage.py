6464c3ed7481e347dc6ca93ccfcad6964456e769
Timothy Chavez <timrchavez@gmail.com>
2014-03-24 16:51:11 -0500

    Add temporary bootstrapping to get the service working with how the charm presently expects to run the app.

--- manage.py
+++ manage.py
@@ -2,6 +2,12 @@
 import os
 import sys
 
+
+# This bootstraps the virtualenv so that the system Python can use it
+app_root = os.path.dirname(os.path.realpath(__file__))
+activate_this = os.path.join(app_root, 'bin', 'activate_this.py')
+execfile(activate_this, dict(__file__=activate_this))
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "capomastro.settings")
 
